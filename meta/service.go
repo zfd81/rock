54d669cc5047c05d74624c24c14a87c1a1f3f9f3
zfd81 <zfd81@189.cn>
2020-08-28 19:15:09 +0800

    完善Parameter类

--- meta/service.go
+++ meta/service.go
@@ -2,13 +2,21 @@ package meta
 
 import "strings"
 
+type ParamType int
+
+const (
+	PathParam = iota
+	RequestParam
+)
+
 type Parameter struct {
-	Name         string `yaml:"name"`
-	Comment      string
-	DataType     string `yaml:"dataType"`
-	Value        interface{}
-	DefaultValue interface{}
-	Index        int
+	Name         string      `yaml:"name"` //参数名称
+	Comment      string      //参数注解
+	DataType     string      `yaml:"dataType"` // 数据类型
+	Value        interface{} // 参数值
+	DefaultValue interface{} // 参数默认值
+	Index        int         `json:"-"` // 参数索引
+	Type         ParamType   `json:"-"` //参数类型
 }
 type Service struct {
 	Name   string       `yaml:"name"`
