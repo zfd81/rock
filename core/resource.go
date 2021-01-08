6a78e1f758aa2067a0c7f21d41c1a6e76704f91c
zfd81 <zfd81@189.cn>
2021-01-08 17:04:35 +0800

    目录结构优化

---
+++ core/resource.go
@@ -0,0 +1,31 @@
+package core
+
+import (
+	"net/http"
+
+	"github.com/zfd81/rock/httpclient"
+	"github.com/zfd81/rock/meta"
+)
+
+type Context interface {
+	GetModule(path string) Module
+	GetDataSource(name string) DB
+	SetHeader(Header http.Header)
+	GetHeader() http.Header
+}
+
+type Resource interface {
+	SetContext(context Context)
+	GetContext() Context
+	GetNamespace() string
+	GetMethod() string
+	GetPath() string
+	GetRegexPath() string
+	GetLevel() int
+	GetPathParams() []*meta.Parameter
+	AddPathParam(param *meta.Parameter)
+	GetRequestParams() []*meta.Parameter
+	AddRequestParam(param *meta.Parameter)
+	Run() (log string, resp *httpclient.Response, err error)
+	Clear()
+}
--- server/services/module.go
+++ server/services/module.go
@@ -1,4 +1,4 @@
-package server
+package services
 
 import (
 	"github.com/zfd81/rock/meta"
