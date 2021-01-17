4a76f737f79acef3d05df1de93b2a2bbd3b90fd3
zfd81 <zfd81@189.cn>
2021-01-17 16:23:02 +0800

    修改jwt的claims类型

--- script/otto/funcs_jwt.go
+++ script/otto/funcs_jwt.go
@@ -7,31 +7,24 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-type RockClaims struct {
-	Data map[string]interface{}
-	jwt.StandardClaims
-}
-
 func CreateToken(data map[string]interface{}, secret string, second int) string {
 	now := time.Now()
-	claims := RockClaims{
-		Data: data,
-		StandardClaims: jwt.StandardClaims{
-			IssuedAt:  now.Unix(),
-			ExpiresAt: now.Add(time.Second * time.Duration(second)).Unix(),
-		},
+	claims := jwt.MapClaims{}
+	for k, v := range data {
+		claims[k] = v
 	}
+	claims["iat"] = now.Unix()                                          //签发日期
+	claims["exp"] = now.Add(time.Second * time.Duration(second)).Unix() //到期时间
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	tokenString, err := token.SignedString([]byte(secret))
 	if err != nil {
 		throwException(err.Error())
 	}
 	return tokenString
-
 }
 
 func ParseToken(tokenString string, secret string) map[string]interface{} {
-	token, err := jwt.ParseWithClaims(tokenString, &RockClaims{}, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
@@ -50,8 +43,8 @@ func ParseToken(tokenString string, secret string) map[string]interface{} {
 			}
 		}
 	}
-	if claims, ok := token.Claims.(*RockClaims); ok && token.Valid {
-		return claims.Data
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims
 	}
 	throwException("Couldn't handle this token")
 	return nil
