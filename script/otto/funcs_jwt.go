87bc5ff21c749f9eef7bbf50862329719ed41549
zfd81 <zfd81@189.cn>
2021-01-27 10:34:22 +0800

    jwt添加RS256的支持

--- script/otto/funcs_jwt.go
+++ script/otto/funcs_jwt.go
@@ -49,3 +49,54 @@ func ParseToken(tokenString string, secret string) map[string]interface{} {
 	throwException("Couldn't handle this token")
 	return nil
 }
+
+func CreateRS256Token(data map[string]interface{}, privatekey string, second int) string {
+	key, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(privatekey))
+	if err != nil {
+		throwException(err.Error())
+	}
+	now := time.Now()
+	claims := jwt.MapClaims{}
+	for k, v := range data {
+		claims[k] = v
+	}
+	claims["iat"] = now.Unix()                                          //签发日期
+	claims["exp"] = now.Add(time.Second * time.Duration(second)).Unix() //到期时间
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	tokenString, err := token.SignedString(key)
+	if err != nil {
+		throwException(err.Error())
+	}
+	return tokenString
+}
+
+func ParseRSAToken(tokenString string, publickey string) map[string]interface{} {
+	key, err := jwt.ParseRSAPublicKeyFromPEM([]byte(publickey)) //解析公钥
+	if err != nil {
+		throwException(err.Error())
+	}
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return key, nil
+	})
+	if err != nil {
+		if ve, ok := err.(*jwt.ValidationError); ok {
+			if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+				throwException("That's not even a token")
+			} else if ve.Errors&jwt.ValidationErrorExpired != 0 {
+				throwException("Token is expired")
+			} else if ve.Errors&jwt.ValidationErrorNotValidYet != 0 {
+				throwException("Token not active yet")
+			} else {
+				throwException("Couldn't handle this token")
+			}
+		}
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims
+	}
+	throwException("Couldn't handle this token")
+	return nil
+}
